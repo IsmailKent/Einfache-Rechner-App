ebe9cb010dd2ff0323e96db111a3607de3c7971e
iwahdan <esmaeel.wahdan@gmail.com>
2022-06-12 11:30:44 +0200

    fix and improve integration tests

--- test/integration.test.js
+++ test/integration.test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = require('assert');
 chai.use(chaiHttp);
+let should = chai.should();
 
 const operations = require('../resources/operations');
 
@@ -18,8 +19,23 @@ describe('Post /calculate', (done) => {
                 parameters: [3, 4]
             }))
             .end((err, res) => {
-                assert(res.status, 200);
-                assert(res.body.result,5);
+                res.should.have.status(200);
+                res.body.result.should.eql(5);
+                done();
+            });
+    });
+    it('Post request to /calculate given invalid parameters returns 400 with right error message ', done => {
+        chai
+            .request(app)
+            .post('/calculate')
+            .set('content-type', 'application/json')
+            .send(JSON.stringify({
+                operationName: "Hypotenuse berechnen",
+                parameters: [3, "a"]
+            }))
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.error.should.eql("Bitte gültige numerische Werte bei allen Feldern eingeben.");
                 done();
             });
     });
@@ -29,10 +45,23 @@ describe('Get /operations', (done) => {
     it('Get request to /operations returns content of operations json object ', done => {
         chai
             .request(app)
-            .get('/opertations')
+            .get('/operations')
             .end((err, res) => {
-                assert(res.status, 200);
-                assert(res.body, operations);
+                console.log("body: \n" +JSON.stringify(res.body));
+                res.should.have.status(200);
+                res.body.should.eql(operations);
+                done();
+            });
+    });
+});
+
+describe('Get /', (done) => {
+    it('Get request to / returns 200 status ', done => {
+        chai
+            .request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
                 done();
             });
     });
